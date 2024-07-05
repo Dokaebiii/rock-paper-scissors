3820fb8e5fc37c26c1bb98e8d1af47a19a009e19
Angel Medina <angelmedina909@yahoo.com>
2024-07-05 11:20:50 -0700

    Add basic rock paper scissors game with no styling

--- JavaScript.js
+++ JavaScript.js
@@ -1,87 +1,97 @@
-
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max) + 1;
 }
 
 function getComputerChoice() {
-    let choice = getRandomInt(3);
-    const rock = "Rock";
-    const paper = "Paper";
-    const scissors = "Scissors";
-
-    if (choice === 0) {
-        return "ROCK";
-    } 
-    else if (choice === 1) {
-        return "PAPER";
-    }
-    else {
-        return "SCISSORS";
-    }
+    return choice = getRandomInt(3);
 }
 
-function getHumanChoice() {
-    let choice = prompt("Choose Rock Paper or Scissors");
-    return choice;
+let computerScore = 0;
+let humanScore = 0;
+let draws = 0;
+
+
+function playRound(hc) {
+    let cc = getComputerChoice();
+    console.log("Computer Choice: " + cc)
+
+    const r = 1;
+    const p = 2;
+    const s = 3;
+
+    if (hc === cc) {
+        draws++;
+        console.log("Draw");
+
+        document.getElementById("draws").textContent = draws;
+    }
+    else if (hc === r && cc === s) {
+        humanScore++;
+        console.log("Rock vs Scissors -> You Win!");
+    }
+    else if (hc === r && cc === p) {
+        computerScore++;
+        console.log("Rock vs Paper -> You Lose.");
+    }
+    else if (hc === p && cc === r) {
+        humanScore++;
+        console.log("Paper vs Rock -> You Win!");
+    }
+    else if (hc === p && cc === s) {
+        computerScore++;
+        console.log("Paper vs Scissors -> You Lose.");
+    }
+    else if (hc === s && cc === p) {
+        humanScore++;
+        console.log("Scissors vs Paper -> You Win!");
+    }
+    else if (hc === s && cc === r) {
+        computerScore++;
+        console.log("Scissors vs Rock -> You Lose.");
+    }
+    else console.log("something happened")
+
+    document.getElementById("wins").textContent = humanScore;
+    document.getElementById("losses").textContent = computerScore;
 }
 
 
+const rockButton = document.querySelector('#rock-button');
+const paperButton = document.querySelector('#paper-button');
+const scissorsButton = document.querySelector('#scissors-button');
 
+rockButton.addEventListener("click", () => {
+    console.log("rock button pressed");
 
-function playGame() {
-    let computerScore = 0;
-    let humanScore = 0;
-    const totalGames = 5;
-    
+    const hc = 1; // rock
+    playRound(hc); 
 
-    function playRound() {
-        let computerChoice = getComputerChoice();
-        let humanChoice = getHumanChoice();
+    console.log("Human Score: " + humanScore);
+    console.log("Computer Score: " + computerScore); 
+});
 
-        let cc = computerChoice;
-        let hc = humanChoice.toUpperCase();
+paperButton.addEventListener("click", () => {
+    console.log("paper button pressed");
 
-        const r = "ROCK";
-        const p = "PAPER";
-        const s = "SCISSORS";
-
-        if (hc === cc) {
-            console.log("Draw");
-        }
-        else if (hc === r && cc === s) {
-            humanScore++;
-            console.log("Rock vs Scissors -> You Win!");
-        }
-        else if (hc === r && cc === p) {
-            computerScore++;
-            console.log("Rock vs Paper -> You Lose.");
-        }
-        else if (hc === p && cc === r) {
-            humanScore++;
-            console.log("Paper vs Rock -> You Win!");
-        }
-        else if (hc === p && cc === s) {
-            computerScore++;
-            console.log("Paper vs Scissors -> You Lose.");
-        }
-        else if (hc === s && cc === p) {
-            humanScore++;
-            console.log("Scissors vs Paper -> You Win!");
-        }
-        else if (hc === s && cc === r) {
-            computerScore++;
-            console.log("Scissors vs Rock -> You Lose.");
-        }
-    }
-
-    for (let i = 0; i < totalGames; i++) {
-        playRound();
-    }
+    const hc = 2; // paper
+    playRound(hc); 
 
     console.log("Human Score: " + humanScore);
     console.log("Computer Score: " + computerScore);
-}
+});
 
-playGame();
+scissorsButton.addEventListener("click", () => {
+    console.log("scissors button pressed");
+
+    const hc = 3; // scissors
+    playRound(hc); 
+
+    console.log("Human Score: " + humanScore);
+    console.log("Computer Score: " + computerScore);
+});
+
+
+
+
 
 
